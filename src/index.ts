ccf73179e8a82c39bd0ca6bf2c0442ba2384ea4a
Arnau Sanchez <pyarnau@gmail.com>
2019-11-18 12:14:32 +0100

    api.currentUser with fields

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,6 @@ export {
     UpdateOptions,
 } from "./api/models";
 export { Id, Ref } from "./schemas/base";
-export { SelectedPick } from "./api/inference";
+export { Selector, SelectedPick } from "./api/inference";
 export { getMockApi, D2ApiMock } from "./testing";
 export { D2ApiResponse } from "./api/common";
