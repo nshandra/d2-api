a3a0627d93384d6b1690403b849cb8bb23250446
Arnau Sanchez <pyarnau@gmail.com>
2020-02-10 15:19:56 +0100

    Add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+/** @format */
+
+module.exports = {
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier/@typescript-eslint",
+        "plugin:prettier/recommended",
+    ],
+    rules: {
+        "no-console": "off",
+        "@typescript-eslint/explicit-function-return-type": ["off"],
+        "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+        "@typescript-eslint/no-use-before-define": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-empty-interface": "off",
+        "@typescript-eslint/ban-ts-ignore": "off",
+        "@typescript-eslint/prefer-interface": "off",
+        "no-dupe-class-members": "off",
+    },
+};
