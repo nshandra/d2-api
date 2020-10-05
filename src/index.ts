2884a1d9822683fc69a64abcf69864166c5ce174
Alexis Rico <sferadev@gmail.com>
2020-10-05 08:44:16 +0200

    Publicly export isCancel method

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,4 @@ export { D2ApiMock, getMockApiFromClass } from "./testing";
 export { ApiContext, useD2, useD2Api } from "./react/context";
 export { useD2ApiData, D2ApiDataHookQuery } from "./react/useD2ApiData";
 export { HttpError } from "./repositories/HttpClientRepository";
-export { Canceler } from "./repositories/CancelableResponse";
+export { Canceler, isCancel } from "./repositories/CancelableResponse";
