10bfae03902bbf91a81a2a2ed93e7bd2c77b760b
Arnau Sanchez <pyarnau@gmail.com>
2022-03-07 11:48:18 +0100

    Export types from events

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 export { D2ApiVersioned, D2ApiGeneric } from "./api/d2Api";
-export { Id, Ref } from "./schemas/base";
 export { D2ApiMock, getMockApiFromClass } from "./testing";
 
 export { ApiContext, useD2, useD2Api } from "./react/context";
 export { useD2ApiData, D2ApiDataHookQuery } from "./react/useD2ApiData";
 export { HttpError } from "./repositories/HttpClientRepository";
 export { Canceler, isCancel } from "./repositories/CancelableResponse";
+export * from "./api/events";
+
+export { CancelableResponse } from "./repositories/CancelableResponse";
+export { Id, Ref, D2Geometry } from "./schemas";
+export * from "./api/events";
